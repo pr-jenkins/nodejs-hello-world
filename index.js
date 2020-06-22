7d5063219c7c8d28264e20c7e4e920c7121763ce
Stanislav Molchanov <molchanovsk@gmail.com>
2020-06-22 10:53:44 +0300

    is it really?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("What a lovely day");
+  response.end("This works");
 });
 
 server.listen(PORT);
